e8baac0a9d849288a91f657ef64c2b87ec3f38a9
JammyJims <jimmy0407t@gmail.com>
2018-02-17 18:14:58 +1030

    hello world, nothing special

--- src/HelloTriangle.cpp
+++ src/HelloTriangle.cpp
@@ -159,7 +159,7 @@ int main()
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
         //glDrawArrays(GL_TRIANGLES, 0, 6);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         // glBindVertexArray(0); // no need to unbind it every time
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
